9f02d158275b3021171135aad771f983b08acf88
mdsaifurrahmann <md.saifurrahmann029@gmail.com>
2024-12-15 23:07:49 +0600

    projects & car app section marked as complete

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,25 @@
 VanillaTilt.init(document.querySelectorAll(".tilt"));
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+        768: {
+            slidesPerView: 3
+        },
+
+        1024: {
+            slidesPerView: 4
+        },
+
+        1280: {
+            slidesPerView: 5
+        }
+    },
+    loop: true,
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -818,9 +818,12 @@ module.exports = {
       150: '1.5',
     },
     screens: {
+      xxs: '375px',
+      xs: '480px',
       sm: '640px',
       md: '768px',
       lg: '1024px',
+      '2lg': '1366px',
       xl: '1280px',
       '2xl': '1536px',
     },
